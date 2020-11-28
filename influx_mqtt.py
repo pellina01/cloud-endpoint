907a1cf2fbd9b09630b59f251547774cbd050e11
jerenz pellina <pellina01@gmail.com>
2020-11-28 17:14:18 +0800

    modify exception

--- influx_mqtt.py
+++ influx_mqtt.py
@@ -5,24 +5,28 @@ class listen:
 
         recieved_list = json.loads(msg.payload.decode("utf-8"))
         self.data = []
-        if recieved_list["status"] == "sending":
-            print("recieved message from: %s =" %
-                  self.topic + recieved_list["value"])
+        try:
+            if recieved_list["status"] == "sending":
+                print("recieved message from: %s =" %
+                      self.topic + recieved_list["value"])
 
-            self.data.append("{measurement},topic={topic} value={value} {timestamp}"
-                             .format(measurement=self.topic, topic=self.topic, value=recieved_list["value"], timestamp=recieved_list["time"]))
+                self.data.append("{measurement},topic={topic} value={value} {timestamp}"
+                                 .format(measurement=self.topic, topic=self.topic, value=recieved_list["value"], timestamp=recieved_list["time"]))
 
-            try:
-                self.influxClient.write_points(
-                    self.data, database=self.database, protocol='line')
-            except Exception as e:
-                print("failed to write to influxdb: %s" % e)
+                try:
+                    self.influxClient.write_points(
+                        self.data, database=self.database, protocol='line')
+                except Exception as e:
+                    print("failed to write to influxdb: %s" % e)
 
-        elif recieved_list["status"] == "connected":
-            print("connected %s" % self.topic)
+            elif recieved_list["status"] == "connected":
+                print("connected %s" % self.topic)
 
-        elif recieved_list["status"] == "disconnected":
-            print("disconnected %s" % self.topic)
+            elif recieved_list["status"] == "disconnected":
+                print("disconnected %s" % self.topic)
+
+        except Exception as e:
+            print("error occured: %s" % e)
 
     def __init__(self, topic, mqtturl, influxHost, database, username, password, influxPort=8086, mqttport=1883, keepalive=60):
         import paho.mqtt.client as mqtt
