fd519382e64aef60ef4cfdd9e1391e8b37c75cf9
jerenz pellina <pellina01@gmail.com>
2020-11-28 15:04:14 +0800

    modify print for debug

--- main.py
+++ main.py
@@ -22,13 +22,16 @@ ph = listen(ph, url, influxHost, database, username, password)
 tb = listen(tb, url, influxHost, database, username, password)
 temp = listen(temp, url, influxHost, database, username, password)
 
+listening = True
 while True:
     try:
-        print("listening..")
-        time.sleep(10)
+        if listening is True:
+            print("listening..")
+            listening = False
     except Exception as e:
         print("error occured: " + traceback.format_exc())
         print("error message: ")
         print(e)
         logging.error(traceback.format_exc())
+        listening = True
         time.sleep(2)
