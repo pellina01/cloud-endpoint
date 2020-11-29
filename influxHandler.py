e504110a5b476e7cea2da980438eea47a3243f66
jerenz pellina <pellina01@gmail.com>
2020-11-29 11:16:39 +0800

    decouple mqtt and influxdb

---
+++ influxHandler.py
@@ -0,0 +1,44 @@
+class handler:
+    def __init__(self, influxHost, username, password, database, topic, influxPort=8086):
+        from influxdb import InfluxDBClient
+
+        self.influxClient = InfluxDBClient(
+            influxHost, influxPort, username, password)
+
+        self.database = database
+        self.topic = topic
+
+        if topic == "ph":
+            self.unit = "pH"
+        elif topic == "tb":
+            self.unit = "NTU"
+        elif topic == "temp":
+            self.unit = "Celsius"
+
+    def dbsend(self, recieved_list):
+        try:
+            self.data = []
+            if recieved_list["status"] == "sending":
+                print("saving message: %s =" % recieved_list["value"])
+
+                self.data.append("{measurement},unit={unit} value={value} {timestamp}"
+                                 .format(measurement=self.topic, unit=self.unit, value=recieved_list["value"], timestamp=recieved_list["time"]))
+
+            elif recieved_list["status"] == "connected":
+                print("connected %s" % self.topic)
+
+            elif recieved_list["status"] == "disconnected":
+                print("disconnected %s" % self.topic)
+
+            try:
+                self.influxClient.write_points(
+                    self.data, database=self.database, protocol='line')
+            except Exception as e:
+                print("failed to write to influxdb: %s" % e)
+
+        except Exception as e:
+            print("failed to write to DB topic %s" % self.topic)
+            print(e)
+
+        finally:
+            del self.data
--- influx_mqtt.py
+++ influx_mqtt.py
@@ -2,47 +2,16 @@ class listen:
 
     def message_callback_add(self, client, userdata, msg):
         import json
+        self.influxHandler.dbsend(json.loads(msg.payload.decode("utf-8")))
 
-        try:
-            self.recieved_list = json.loads(msg.payload.decode("utf-8"))
-            if self.recieved_list["status"] == "sending":
-                print("recieved message from: %s =" %
-                      self.topic + self.recieved_list["value"])
-
-                self.data = []
-                self.data.append("{measurement},topic={topic} value={value} {timestamp}"
-                                 .format(measurement=self.topic, topic=self.topic, value=self.recieved_list["value"], timestamp=self.recieved_list["time"]))
-
-                try:
-                    self.influxClient.write_points(
-                        self.data, database=self.database, protocol='line')
-                except Exception as e:
-                    print("failed to write to influxdb: %s" % e)
-
-            elif self.recieved_list["status"] == "connected":
-                print("connected %s" % self.topic)
-
-            elif self.recieved_list["status"] == "disconnected":
-                print("disconnected %s" % self.topic)
-
-        except Exception as e:
-            print("error occured: %s " % e)
-            print("recieved value: %s" % msg.payload.decode("utf-8"))
-
-        finally:
-            del self.recieved_list
-            del self.data
-
-    def __init__(self, topic, mqtturl, influxHost, database, username, password, influxPort=8086, mqttport=1883, keepalive=60):
+    def __init__(self, topic, mqtturl, influxHost, database, username, password, influxPort, mqttport=1883, keepalive=60):
         import paho.mqtt.client as mqtt
-        from influxdb import InfluxDBClient
+        from influxHandler import handler
+
+        self.influxHandler = handler(
+            influxHost, username, password, database, topic)
 
         self.topic = topic
-        self.database = database
-
-        self.influxClient = InfluxDBClient(
-            influxHost, influxPort, username, password)
-
         self.mqttClient = mqtt.Client()
 
         self.connected = False
