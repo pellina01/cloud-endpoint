fda9ddb954b494cc86bdaaf7c2271e73c75bd793
jerenz pellina <pellina01@gmail.com>
2020-11-29 11:24:39 +0800

    decouple mqtt and influxdb

--- mqttHandler.py
+++ mqttHandler.py
@@ -2,7 +2,10 @@ class listen:
 
     def message_callback_add(self, client, userdata, msg):
         import json
-        self.influxHandler.dbsend(json.loads(msg.payload.decode("utf-8")))
+        try:
+            self.influxHandler.dbsend(json.loads(msg.payload.decode("utf-8")))
+        except Exception as e:
+            print("error occured: %" % e)
 
     def __init__(self, topic, mqtturl, influxHost, database, username, password, influxPort=8086, mqttport=1883, keepalive=60):
         import paho.mqtt.client as mqtt
