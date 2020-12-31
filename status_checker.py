2596ffdce9e4437fc87009892dd9bf1d55280d98
jerenz pellina <pellina01@gmail.com>
2020-12-31 12:15:45 +0800

    try new code for influxdb

--- status_checker.py
+++ status_checker.py
@@ -4,7 +4,7 @@ class status_checker:
 
     def isValid(self, recieved_list):
         checker = recieved_list["status"]
-        return getattr(status_checker, checker)
+        return getattr(status_checker, checker)(self)
 
     def connected(self):
         self.counter += 1
@@ -14,6 +14,8 @@ class status_checker:
         if self.counter > 0:
             self.counter -= 1
             return True if self.counter == 0 else False
+        else:
+            return False
 
     def sending(self):
         return True
