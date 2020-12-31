675f5b3e127c4cc6e270cfd17ec11a1bb40dec2b
jerenz pellina <pellina01@gmail.com>
2020-12-31 15:30:34 +0800

    try new code for influxdb

--- serializer.py
+++ serializer.py
@@ -19,7 +19,7 @@ class serializer:
 
     def sending(self, recieved_list):
         float(recieved_list["value"])
-        return self.influx_serializer(self.topic, self.unit, recieved_list["value"])
+        return self.influx_serializer(self.topic, self.unit, str(recieved_list["value"]))
 
     def connected(self, recieved_list):
         return self.influx_serializer("{}_status".format(self.topic), self.unit, "connected")
